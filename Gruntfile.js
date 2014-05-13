17adad67804235ec0fa59de807ce7df330f1b08d
Tim Hemming <timhemming@gmail.com>
2014-05-13 06:37:56 +0100

    Project maintenance.

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function(grunt) {
           }
         },
         files: {
-          'build/my-api.js': ['src/my-api/**/*.js'],
+          'build/my-api.js': ['src/my-api/**/*.js']
         }
       }
     }
