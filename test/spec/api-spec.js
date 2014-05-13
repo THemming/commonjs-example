ee8e7879741425c1c327a557e6587e453de97641
Tim Hemming <timhemming@gmail.com>
2014-05-13 06:37:11 +0100

    Tests now cover multiple instances of the same module, with and without mocks.

--- test/spec/api-spec.js
+++ test/spec/api-spec.js
@@ -1,31 +1,83 @@
-var Api = require('../../src/my-api/my-api', {
-    '../../src/my-api/rest-client': function() {
-        this.getName = function() {
-            return 'mock rest client';
-        };
+describe('my-api module', function () {
 
-        this.getJquery = function() {
-            return 'mock jquery';
-        };
-    }
-});
+    var API_MODULE_PATH = '../../src/my-api/my-api';
 
-describe('api module', function() {
-    var api;
+    describe('without mocks', function () {
 
-    beforeEach(function() {
-        api = new Api();
+        var api;
+        var Api = require(API_MODULE_PATH);
+
+        beforeEach(function () {
+            api = new Api();
+        });
+
+        it('has a name', function () {
+            expect(api.getName()).toEqual('my-api');
+        });
+
+        it('has a rest instance with a name', function () {
+            expect(api.getRest().getName()).toEqual('rest-client module');
+        });
+
+        it('rest instance can get jquery reference', function () {
+            expect(api.getRest().getJquery()).toEqual(jasmine.any(Function));
+        });
+
     });
 
-    it('has a name', function() {
-        expect(api.getName()).toEqual('my-api');
+    describe('with mocks', function () {
+
+        var api;
+        var ApiWithMocks = require(API_MODULE_PATH, {
+            '../../src/my-api/rest-client': function () {
+                this.getName = function () {
+                    return 'mock rest client';
+                };
+                this.getJquery = function () {
+                    return 'mock jquery';
+                };
+            }
+        });
+
+        beforeEach(function () {
+            api = new ApiWithMocks();
+        });
+
+        it('has a name', function () {
+            expect(api.getName()).toEqual('my-api');
+        });
+
+        it('has a rest instance with a name', function () {
+            expect(api.getRest().getName()).toEqual('mock rest client');
+        });
+
+        it('rest instance can get jquery reference', function () {
+            expect(api.getRest().getJquery()).toEqual('mock jquery');
+        });
+
     });
 
-    it('has a rest instance with a name', function() {
-        expect(api.getRest().getName()).toEqual('mock rest client');
+    describe('with deep mock', function () {
+
+        var api;
+        var ApiWithMocks = require(API_MODULE_PATH, {
+            '../../src/my-api/vendor-libs': {
+                jQuery: 'deep mock jquery'
+            }
+        });
+
+        beforeEach(function () {
+            api = new ApiWithMocks();
+        });
+
+        it('has a rest instance with a name', function () {
+            expect(api.getRest().getName()).toEqual('rest-client module');
+        });
+
+        it('rest instance can get jquery reference', function () {
+            expect(api.getRest().getJquery()).toEqual('deep mock jquery');
+        });
+
     });
 
-    it('rest instance can get jquery reference', function() {
-        expect(api.getRest().getJquery()).toEqual('mock jquery');
-    });
 });
